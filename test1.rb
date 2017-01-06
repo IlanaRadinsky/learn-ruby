808f382b6fe6966c904efadb2fddf4eec9c741c7
Ilana Radinsky <ilanaradinsky@gmail.com>
2017-01-05 22:36:54 -0500

    learned times

--- test1.rb
+++ test1.rb
@@ -398,3 +398,39 @@ puts "#{H["b"]}"
 
 tester = Hash[[1,"jan"]=>"January"] #can use any Ruby object as a key or value
 puts "#{tester[[1,"jan"]]}"
+
+#----- DATE AND TIME ------
+time1 = Time.new #or Time.now 
+
+puts "Current Time: " + time1.inspect #returns current time
+
+time = Time.new
+
+# Components of a Time
+puts "Current Time : " + time.inspect
+puts time.year    # => Year of the date 
+puts time.month   # => Month of the date (1 to 12)
+puts time.day     # => Day of the date (1 to 31 )
+puts time.wday    # => 0: Day of week: 0 is Sunday
+puts time.yday    # => 365: Day of year
+puts time.hour    # => 23: 24-hour clock
+puts time.min     # => 59
+puts time.sec     # => 59
+puts time.usec    # => 999999: microseconds
+puts time.zone    # => "UTC": timezone name
+
+# July 8, 2008
+Time.local(2008, 7, 8)  
+# July 8, 2008, 09:10am, local time
+Time.local(2008, 7, 8, 9, 10)   
+# July 8, 2008, 09:10 UTC
+Time.utc(2008, 7, 8, 9, 10)  
+# July 8, 2008, 09:10:11 GMT (same as UTC)
+Time.gm(2008, 7, 8, 9, 10, 11)  
+
+time = Time.new
+
+puts "#{time.to_a}" #returns array in form: [sec,min,hour,day,month,year,wday,yday,isdst,zone]
+					#can then pass this array to time.utc to get it in UTC format
+
+puts "#{Time.now.to_i}"					
