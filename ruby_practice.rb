ba3c1dacb817d7983c7658c4478950f5a812c1b4
Ilana Radinsky <ilanaradinsky@gmail.com>
2017-01-10 20:57:20 -0500

    Practicing Ruby using Python Proficiency Test

--- ruby_practice.rb
+++ ruby_practice.rb
@@ -30,3 +30,72 @@ for i in (0..4)
 
 	puts "x"
 end
+
+def cylinderSurfaceArea(radius, height)
+	@r = radius
+	@h = height
+	2*Math::PI*(@r**2)+2*Math::PI*@r*@h
+end
+
+puts "#{cylinderSurfaceArea(1,1)}"
+
+def drawBox(size)
+	for i in (0...size)
+		for j in (0...size)
+			if (((i==1) || (i==size-2)) && (1..size-2).include?(j)) || (((j==1) || (j==size-2)) && (1..size-2).include?(i))
+				print "x"
+			else
+				print "-"
+			end
+		end
+		puts ''
+	end
+end
+
+drawBox(10)
+
+def addOddStrings(num)
+	final = ''
+	num.each do |str|
+		if str.length%2 != 0
+			final = final + str
+		end
+	end
+
+	puts final
+end
+
+addOddStrings(["Hello", "World", "it's very nice", "to", "meet you!"])
+
+def count613(nums)
+	lastnum = 0
+	count = 0
+	nums.each do |num|
+		if (lastnum == 6 && num == 13) || (lastnum == 61 && num == 3)
+			# puts "YES!"
+			count += 1
+		end
+		lastnum = num
+	end
+	puts "#{count}"
+end
+
+count613([6, 13, 2, 61, 3, 3, 9, 61, 3, 6, 13])
+
+def everyOther(items)
+	on = 1
+	ind = 0
+	result = Array.new
+	items.each do |item|
+		if on%2 != 0
+			result[ind] = item
+			ind += 1
+			puts "#{item}"
+		end
+		# ind +=1
+		on +=1
+	end
+	puts "#{result}"
+end
+
+everyOther([2,4,6,8,10,12,14])
